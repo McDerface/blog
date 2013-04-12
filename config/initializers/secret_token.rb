3e3ef86947e80e1f3e4b2e63cf05b6a7f22a73a8
Timothy McDermott <tmcdermott@retailcontrolsystems.com>
2013-04-11 20:54:07 -0400

    Initial Commitmentgit status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '966918c67c5168314dcd7dc45bac4f17f5e392c58355c6c6d0d0bcafae5b461ffc96a49ac405f7d69c0e945fc2502d7990b242814cb0eb28523c0bb7aaddd1df'
